63113af2741a7adda994b5ee813c313354098b34
Kim DI <daein7076@gmail.com>
2018-09-27 16:56:09 +0900

    r'^$'패턴과 blog.view.post_list view function연결

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
 
-# Create your views here.
+
+def post_list(request):
+    #현재 지역에 맞는 날짜&시간 객체 할당
+    current_time = timezone.now()
+    '''
+
+    :param request:실제 HTTP요청에 대한 정보를 가진 객체
+    :return:
+    '''
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List</h1>'
+        '<p>{}</p>'
+        '</body>'
+        '</html>'.format(
+            #날짜 & 시간 객체가 가진 정보를 문자열로 변환
+            current_time.strftime('%Y, %m, %d<br>%H:%M:%S')
+        )
+    )
