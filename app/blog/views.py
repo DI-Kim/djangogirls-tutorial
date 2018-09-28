fc54655bfb779dfa2230955353d29c9ac48d5d93
Kim DI <daein7076@gmail.com>
2018-09-28 11:25:46 +0900

    post_list view 에서 template을 loader를 통해 가져와 render한 결과를 리턴

--- app/blog/views.py
+++ app/blog/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponse
 # from django.utils import timezone
 import os
 
-
-
-
+from django.template import loader
 
 
 def post_list(request):
@@ -21,16 +19,17 @@ def post_list(request):
     # os.path .dirname(<경로>)   <- 특정 경로의 상위폴더로 이동
     # os.path.join(<경로>, <폴더/파일명>) <- 특정 경로에서 하위폴더 또는 하위 파일명을 나타냄
 
-    current_path = os.path.abspath(__file__)
-    parent_dir = os.path.dirname(current_path)
-    # app_dir = os.path.dirname(parent_dir)
-    template_path = os.path.join(parent_dir, 'templates', 'blog', 'post_list.html')
-    #
+    # current_path = os.path.abspath(__file__)
+    # parent_dir = os.path.dirname(current_path)
+    # template_path = os.path.join(parent_dir, 'templates', 'blog', 'post_list.html')
     # with open(template_path, 'rt') as f:
     #     content = f.read()
-
-    content = open(template_path, 'rt').read()
-
-
+    # content = open(template_path, 'rt').read()
+#####################################
+    # 템플릿 가져옴 (단순 문자열이 아님)
+    template = loader.get_template('blog/post_list.html')
+    # 해당 템플릿을 렌더링
+    context = {}
+    content = template.render(context, request)
 
     return HttpResponse(content)
